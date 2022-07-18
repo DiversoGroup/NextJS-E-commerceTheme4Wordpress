1be1738f08c0cf4b1247d81b3c5358877e205c60
carlos bult <acevedo.bult@gmail.com>
2022-07-18 14:58:19 -0430

    queries and mutation folders

--- client/src/pages/blog/index.js
+++ client/src/pages/blog/index.js
@@ -1,14 +1,31 @@
-// import Head from 'next/head';
-// import Link from 'next/link';
+import client from '../../apollo/client';
+import { GET_MENUS } from '../../apollo/queries/getMenus';
 
-// // data
-// import { getAllPosts } from '../../lib_/api.js.sa';
+function Blog({ data }) {
+  const { headerMenus, footerMenus } = data || {};
 
-// // styles
-// // import { GlobalStyle } from '../../assets/styles/globals.css';
+  const { edges } = headerMenus || {};
 
-// function Blog({ allPosts: { edges } }) {
-//   return <h1>Hola Blog</h1>;
-// }
+  console.log(headerMenus);
 
-// export default Blog;
+  return (
+    <div className="flex h-screen min-h-[660px] flex-col items-center justify-center space-y-[9vh] overflow-hidden bg-hero-pattern bg-cover bg-center bg-no-repeat px-8">
+      <h1 className="text-red-400">Hola Blog</h1>
+      <h1>{edges[0].node.label}</h1>
+    </div>
+  );
+}
+
+export default Blog;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: GET_MENUS,
+  });
+
+  return {
+    props: {
+      data: data || {},
+    },
+  };
+}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,57 +7,7 @@
 import Login from './Login';
 
 function Home() {
-  // fetch(process.env.NEXT_PUBLIC_WP_API_URL, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     query: `
-  //     query AllPosts {
-  //       posts {
-  //         edges {
-  //           node {
-  //             databaseId
-  //             date
-  //             title
-  //             slug
-  //           content
-  //           }
-  //           }
-  //       }
-  //     }
-  //     `,
-  //   }),
-  // })
-  //   .then((res) => res.json())
-  //   .then((res) => console.log(res.data));
-
-  return (
-    // <Catalogue />
-    // <CardDetails />
-    // <Landing />;
-    <Login />
-  );
+  return <Login />;
 }
 
 export default Home;
-
-// Fetch from WordPress API
-// const fetchAPI = async (query) => {
-//   const response = await fetch(process.env.NEXT_PUBLIC_WP_API_URL, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Accept: 'application/json',
-//     },
-//     body: JSON.stringify({ query }),
-//   });
-
-//   const json = await response.json();
-
-//   return json;
-// };
-
-// console.log('Se ve');
-// console.log(fetchAPI);
