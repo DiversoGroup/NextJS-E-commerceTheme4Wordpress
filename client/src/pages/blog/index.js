b259ff5671dcd2fdeacf490320dbde19c92cff20
carlos bult <acevedo.bult@gmail.com>
2022-07-18 16:17:02 -0430

    .

--- client/src/pages/blog/index.js
+++ client/src/pages/blog/index.js
@@ -11,7 +11,7 @@ function Blog({ data }) {
   return (
     <div className="flex h-screen min-h-[660px] flex-col items-center justify-center space-y-[9vh] overflow-hidden bg-hero-pattern bg-cover bg-center bg-no-repeat px-8">
       <h1 className="text-red-400">Hola Blog</h1>
-      <h1>{edges[0].node.label}</h1>
+      <h1>{edges[2].node.label}</h1>
     </div>
   );
 }
